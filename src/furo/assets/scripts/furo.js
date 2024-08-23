8c9e7dc98884a9efd73017029d57f06ec3c5b0a4
bmorelli25 <brandon.morelli@elastic.co>
2024-08-22 18:44:53 -0700

    customize

--- src/furo/assets/scripts/furo.js
+++ src/furo/assets/scripts/furo.js
@@ -176,3 +176,32 @@ function main() {
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
+//
+// Custom header
+//
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+      toggle.classList.toggle("bx-x");
+    });
+  }
+};
+
+showMenu("header-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+}
